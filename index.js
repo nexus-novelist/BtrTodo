7539f5ebb2d4f26d62855c0a1846baf36c80a816
ProNoice <robex.omar@gmail.com>
2024-02-16 22:56:54 +0200

    v1.1.0 - Added Editing

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const inputBox = document.getElementById("input-box");
 
 let todos = JSON.parse(localStorage.getItem("todo-list"));
 
+let isEditing = false,
+curEditTask = undefined;
+
 function generateUUID() {
     return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c =>
       (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
@@ -25,10 +28,15 @@ function addTodo(text, id){
     }else{
         let todo = document.createElement("li");
         todo.id = id;
-        todo.textContent = text;
+        let todoText = document.createElement("p");
+        todoText.textContent = text;
+        todo.appendChild(todoText);
         let deleteBtn = document.createElement("span");
-        deleteBtn.innerHTML = `<i class="fa-solid fa-trash-can"></i>`;
+        deleteBtn.innerHTML = `<i class="fa-solid fa-trash-can delete-btn"></i>`;
         todo.appendChild(deleteBtn);
+        let editBtn = document.createElement("span");
+        editBtn.innerHTML = `<i class="fa-solid fa-pen edit-btn"></i>`;
+        todo.appendChild(editBtn);
         const task = getTaskById(id);
         if(todos.find((value) => value == task).checked){
             todo.classList.add("checked");
@@ -46,13 +54,17 @@ function checkTodo(id){
 }
 
 function createTodo(){
-    const uuid = generateUUID();
-    todos = !todos ? [] : todos;
-    let taskInfo = {text: inputBox.value, id: uuid, checked: false};
-    todos.push(taskInfo);
-    localStorage.setItem("todo-list", JSON.stringify(todos));
-    addTodo(inputBox.value, uuid);
-    inputBox.value = "";
+    if(!isEditing){
+        const uuid = generateUUID();
+        todos = !todos ? [] : todos;
+        let taskInfo = {text: inputBox.value, id: uuid, checked: false};
+        todos.push(taskInfo);
+        localStorage.setItem("todo-list", JSON.stringify(todos));
+        addTodo(inputBox.value, uuid);
+        inputBox.value = "";
+    }else{
+        editTask();
+    }
 }
 
 function deleteTask(id) {
@@ -61,6 +73,23 @@ function deleteTask(id) {
     todos.splice(i, 1);
     localStorage.setItem("todo-list", JSON.stringify(todos));
     document.getElementById(id).remove();
+}
+
+function editTask(id) {
+    if(!isEditing){
+        const task = getTaskById(id);
+        curEditTask = task;
+        isEditing = true;
+        inputBox.value = task.text;
+    }else{
+        const i = todos.indexOf(curEditTask);
+        todos[i].text = inputBox.value;
+        localStorage.setItem("todo-list", JSON.stringify(todos));
+        inputBox.value = "";
+        document.getElementById(curEditTask.id).childNodes[0].textContent = todos[i].text;
+        isEditing = false;
+        curEditTask = undefined;
+    }
 }
 
 function clearAll() {
@@ -83,14 +112,21 @@ listContainer.addEventListener("click", (e) => { //check todo
     if(e.target.tagName == "LI"){
         const id = e.target.id;
         checkTodo(id);
-    }else if(e.target.tagName == "I") {
+    }else if(e.target.classList.contains("delete-btn")) {
         const id = e.target.parentElement.parentElement.id;
         deleteTask(id);
+    }else if(e.target.classList.contains("edit-btn")) {
+        const id = e.target.parentElement.parentElement.id;
+        editTask(id);
     }
 })
 
 inputBox.onkeyup = ((e) => { //create todo by pressing enter
     if(e.key == "Enter") {
-        createTodo();
+        if(!isEditing){
+            createTodo();
+        }else{
+            editTask();
+        }
     }
 });
